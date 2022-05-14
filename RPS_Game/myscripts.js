dcc1da55ebff0fda30043bffa4d5e474889e563d
Oktay Cem Kutlar <cemkutlar333@gmail.com>
2022-05-14 19:58:51 +0300

    Buttons added, user can click buttons as an answer.

--- RPS_Game/myscripts.js
+++ RPS_Game/myscripts.js
@@ -23,19 +23,48 @@ function playRound(playerSelection, computerSelection) {
     
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let answer = prompt("Rock, paper or scissor?");
-        let computerSelection = computerPlay();
-        playRound(answer,computerSelection);
-        console.log("Your Point: " + playerPoint + "\n" + "Bots Point: " + computerPoint);
-        
-    }
-    if(playerPoint > computerPoint) console.log("You Win!!!");
-    else if(playerPoint < computerPoint) console.log("You Lost!!!");
-    else console.log("DRAW!");
-}
+function game(element) {
+    
+    //let answer = prompt("Rock, paper or scissor?");
+    
+    //console.log(ps_buttons);
+    
+    let computerSelection = computerPlay();
+    playRound(element.innerText, computerSelection);
 
+    //console.log(answer);
+    // let computerSelection = computerPlay();
+    // playRound(answer,computerSelection);
+    if(computerPoint == 0 && playerPoint == 0) console.log("Game Start")
+    else console.log("Your Point: " + playerPoint + "\n" + "Bots Point: " + computerPoint);
+        
+    if(computerPoint == 3) {
+        console.log("YOU LOST THE GAME!");
+        playerPoint = computerPoint = 0;
+        return;
+    }
+    if(playerPoint == 3) {
+        console.log("YOU WON THE GAME!")
+        playerPoint = computerPoint = 0;
+        return;
+    }
+    if(playerPoint > tempPlayerPoint) console.log("You won this round!!!");
+    else if(computerPoint > tempCompPoint) console.log("You lost this round!!!");
+    else console.log("DRAW!");
+    tempPlayerPoint = playerPoint;
+    tempCompPoint = computerPoint;
+    
+}
+const ps_buttons = document.querySelector(".ps_buttons");
+let answers = ps_buttons.querySelectorAll("button");
+let tempPlayerPoint = 0;
+let tempCompPoint = 0;
 let playerPoint = 0;
 let computerPoint = 0;
-game();
+answers.forEach(element => {
+    element.addEventListener("click", function(){
+        game(element);
+    }
+)});
+
+
